f06a957425486d41e64e4b38c68f09cfe0647831
mariafjesus <mariafjesus4@gmail.com>
2024-07-15 12:11:30 +0100

    send data to Flask web server

--- Assets/Scripts/SendData.cs
+++ Assets/Scripts/SendData.cs
@@ -6,13 +6,15 @@ using System.Text;
 
 public class SendData : MonoBehaviour
 {
-    private string url = "http://127.0.0.1:5000/receive_data";
+    private string url = "http://127.0.0.1:5000/receive_data"; // Flask server URL
 
     // Example data to send
     private Dictionary<string, string> data = new Dictionary<string, string>()
     {
-        { "name", "John Doe" },
-        { "score", "100" }
+        { "wifi_name", "My Wifi" },
+        { "wifi_strength", "-37dBm" },
+        { "wifi_speed", "120Mbps" },
+        { "img_url", "https://dl3.pushbulletusercontent.com/6QIA6AZNTPRTjG5KKNDqTmQT6SYMtyOx/image.png" }
     };
 
     void Start()
@@ -22,7 +24,7 @@ public class SendData : MonoBehaviour
     
     IEnumerator SendDataToServer()
     {
-        string jsonData = JsonUtility.ToJson(data);
+        string jsonData = JsonUtility.ToJson(new DataWrapper(data));
 
         UnityWebRequest www = new UnityWebRequest(url, "POST");
         byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonData);
@@ -39,6 +41,24 @@ public class SendData : MonoBehaviour
         else
         {
             Debug.Log("Response: " + www.downloadHandler.text);
+        }
+    }
+
+    // Wrapper class to convert Dictionary to JSON correctly
+    [System.Serializable]
+    private class DataWrapper
+    {
+        public string wifi_name;
+        public string wifi_strength;
+        public string wifi_speed;
+        public string img_url;
+
+        public DataWrapper(Dictionary<string, string> data)
+        {
+            this.wifi_name = data["wifi_name"];
+            this.wifi_strength = data["wifi_strength"];
+            this.wifi_speed = data["wifi_speed"];
+            this.img_url = data["img_url"];
         }
     }
 }
