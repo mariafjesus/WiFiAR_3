96bcd344d52127c15554ca1a86a15a458673b9fa
mariafjesus <mariafjesus4@gmail.com>
2024-07-26 11:36:25 +0100

    delete walls from other wall groups

--- Assets/Scripts/RoomScanner.cs
+++ Assets/Scripts/RoomScanner.cs
@@ -14,17 +14,16 @@ public class RoomScanner : MonoBehaviour
     public OVRInput.Button deletePreviousButton = OVRInput.Button.SecondaryHandTrigger;
     public OVRInput.Button interruptLineButton = OVRInput.Button.One; // Button to interrupt the current line
 
-    private List<Vector3> points = new List<Vector3>(); // List to store points
+    private List<List<Vector3>> pointGroups = new List<List<Vector3>>(); // List of lists to store points for each group
     private List<Vector3> allPoints = new List<Vector3>(); // List to store all points
     private List<GameObject> placedCubes = new List<GameObject>(); // List to store placed cube instances
     private List<WallConnection> wallConnections = new List<WallConnection>(); // List to store wall connections
     private List<LineRenderer> lineRenderers = new List<LineRenderer>(); // List to store line renderers
     private List<LineRenderer> mapLineRenderers = new List<LineRenderer>(); // List to store map line renderers
 
-
     private LineRenderer currentLineRenderer; // Current line renderer
     private LineRenderer currentMapLineRenderer; // Current thick line renderer
-
+    private List<Vector3> currentPoints; // Points for the current group
 
     private class WallConnection
     {
@@ -58,12 +57,12 @@ public class RoomScanner : MonoBehaviour
                 Vector3 snappedPoint = GetSnappedPoint(hitPoint);
 
                 // Check if the new point is not repeated
-                if (points.Count == 0 || snappedPoint != points[points.Count - 1])
+                if (currentPoints.Count == 0 || snappedPoint != currentPoints[currentPoints.Count - 1])
                 {
                     PlacePoint(snappedPoint);
                 }
             }
-        } 
+        }
         else if (OVRInput.GetDown(deletePreviousButton))
         {
             RemoveLastPoint();
@@ -89,30 +88,28 @@ public class RoomScanner : MonoBehaviour
     void PlacePoint(Vector3 hitPoint)
     {
         GameObject newCube = Instantiate(cube, hitPoint, Quaternion.identity); // Create a new cube
-        points.Add(hitPoint);
+        currentPoints.Add(hitPoint);
         allPoints.Add(hitPoint);
         placedCubes.Add(newCube);
 
-        // Place wall if there are at least 2 points
-        if (points.Count > 1)
+        // Place wall if there are at least 2 points in the current group
+        if (currentPoints.Count > 1)
         {
-            PlaceWall(points[points.Count - 2], points[points.Count - 1]);
+            PlaceWall(currentPoints[currentPoints.Count - 2], currentPoints[currentPoints.Count - 1]);
         }
 
         // Update the current line renderer with the new point
-        currentLineRenderer.positionCount = points.Count;
-        currentLineRenderer.SetPositions(points.ToArray());
+        UpdateLineRenderer(currentLineRenderer, currentPoints);
 
-        // Create the map lines
-        currentMapLineRenderer.positionCount = points.Count;
-        currentMapLineRenderer.SetPositions(points.ToArray());
+        // Update the current map line renderer with the new point
+        UpdateLineRenderer(currentMapLineRenderer, currentPoints);
     }
 
     void PlaceWall(Vector3 startPoint, Vector3 endPoint)
     {
-        Vector3 wallPosition = (startPoint + endPoint) / 2 + new Vector3(0,0.6f,0);
+        Vector3 wallPosition = (startPoint + endPoint) / 2 + new Vector3(0, 0.6f, 0);
         Vector3 direction = endPoint - startPoint;
-        Quaternion wallRotation = Quaternion.LookRotation(direction) * Quaternion.Euler(0,90,0);
+        Quaternion wallRotation = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 90, 0);
 
         GameObject newWall = Instantiate(wallPrefab, wallPosition, wallRotation);
         newWall.transform.localScale = new Vector3(direction.magnitude, 1.2f, 1); // Scale wall to fit between points
@@ -133,36 +130,62 @@ public class RoomScanner : MonoBehaviour
 
     void RemoveLastPoint()
     {
-        // Undo action
-        if (points.Count > 0)
+        if (currentPoints.Count > 0)
         {
-            // Remove the last point and the corresponding cube
-            Vector3 lastPoint = points[points.Count - 1];
-            points.RemoveAt(points.Count - 1);
-            allPoints.RemoveAt(allPoints.Count - 1);
+            // Remove the last point from the current group
+            Vector3 lastPoint = currentPoints[currentPoints.Count - 1];
+            currentPoints.RemoveAt(currentPoints.Count - 1);
+            allPoints.Remove(lastPoint);
+
+            // Remove the corresponding cube
             GameObject lastCube = placedCubes[placedCubes.Count - 1];
             Destroy(lastCube);
             placedCubes.RemoveAt(placedCubes.Count - 1);
 
             // Remove the corresponding wall
-            for (int i = wallConnections.Count - 1; i >= 0; i--)
+            if (currentPoints.Count > 0)
             {
-                if (wallConnections[i].Point1 == lastPoint || wallConnections[i].Point2 == lastPoint)
+                Vector3 previousPoint = currentPoints[currentPoints.Count - 1];
+                for (int i = wallConnections.Count - 1; i >= 0; i--)
                 {
-                    Destroy(wallConnections[i].Wall);
-                    wallConnections.RemoveAt(i);
-                    break;
+                    if ((wallConnections[i].Point1 == lastPoint && wallConnections[i].Point2 == previousPoint) ||
+                        (wallConnections[i].Point1 == previousPoint && wallConnections[i].Point2 == lastPoint))
+                    {
+                        Destroy(wallConnections[i].Wall);
+                        wallConnections.RemoveAt(i);
+                        break;
+                    }
                 }
             }
 
             // Update the current line renderer
-            currentLineRenderer.positionCount = points.Count;
-            currentLineRenderer.SetPositions(points.ToArray());
+            UpdateLineRenderer(currentLineRenderer, currentPoints);
 
-            currentMapLineRenderer.positionCount = points.Count;
-            currentMapLineRenderer.SetPositions(points.ToArray());
+            // Update the current map line renderer
+            UpdateLineRenderer(currentMapLineRenderer, currentPoints);
+        }
+        else if (pointGroups.Count > 0)
+        {
+            // Remove the last empty group and its corresponding line renderers
+            pointGroups.RemoveAt(pointGroups.Count - 1);
+            LineRenderer lastLineRenderer = lineRenderers[lineRenderers.Count - 1];
+            LineRenderer lastMapLineRenderer = mapLineRenderers[mapLineRenderers.Count - 1];
+            lineRenderers.RemoveAt(lineRenderers.Count - 1);
+            mapLineRenderers.RemoveAt(mapLineRenderers.Count - 1);
+            Destroy(lastLineRenderer.gameObject);
+            Destroy(lastMapLineRenderer.gameObject);
 
-            Debug.Log("Removed the last point");
+            if (pointGroups.Count > 0)
+            {
+                // Set the current points to the last non-empty group
+                currentPoints = pointGroups[pointGroups.Count - 1];
+                currentLineRenderer = lineRenderers[lineRenderers.Count - 1];
+                currentMapLineRenderer = mapLineRenderers[mapLineRenderers.Count - 1];
+            }
+            else
+            {
+                CreateNewLineRenderer();
+            }
         }
     }
 
@@ -184,7 +207,14 @@ public class RoomScanner : MonoBehaviour
 
         currentMapLineRenderer.gameObject.layer = LayerMask.NameToLayer("Rooms");
 
-        // Clear points list to start a new line segment
-        points.Clear();
+        // Start a new group of points
+        currentPoints = new List<Vector3>();
+        pointGroups.Add(currentPoints);
+    }
+
+    void UpdateLineRenderer(LineRenderer lineRenderer, List<Vector3> points)
+    {
+        lineRenderer.positionCount = points.Count;
+        lineRenderer.SetPositions(points.ToArray());
     }
 }
