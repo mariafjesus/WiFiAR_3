fb2809c27a81b1984ae3b92ef2d4a63d2baec064
mariafjesus <mariafjesus4@gmail.com>
2024-07-30 11:15:46 +0100

    show cube preview before placing wall

--- Assets/Scripts/RoomScanner.cs
+++ Assets/Scripts/RoomScanner.cs
@@ -5,6 +5,7 @@ public class RoomScanner : MonoBehaviour
 {
     public Transform origin; // The transform from which to cast the ray
     public GameObject cube; // The cube prefab to instantiate
+    public GameObject previewCubePrefab;
     public GameObject wallPrefab; // The wall prefab to instantiate
     public Material lineMaterial; // Material for the connecting lines
     public Material mapLineMaterial; // Material for map lines
@@ -25,6 +26,8 @@ public class RoomScanner : MonoBehaviour
     private LineRenderer currentMapLineRenderer; // Current thick line renderer
     private List<Vector3> currentPoints; // Points for the current group
 
+    private GameObject previewCube;
+
     private class WallConnection
     {
         public Vector3 Point1;
@@ -42,6 +45,7 @@ public class RoomScanner : MonoBehaviour
     void Start()
     {
         CreateNewLineRenderer();
+        CreatePreviewCube();
     }
 
     void Update()
@@ -70,6 +74,9 @@ public class RoomScanner : MonoBehaviour
         else if (OVRInput.GetDown(interruptLineButton))
         {
             CreateNewLineRenderer();
+        }
+        else {
+            UpdatePreviewCubePosition();
         }
     }
 
@@ -186,6 +193,29 @@ public class RoomScanner : MonoBehaviour
             {
                 CreateNewLineRenderer();
             }
+        }
+    }
+
+    void CreatePreviewCube()
+    {
+        previewCube = Instantiate(previewCubePrefab);
+        previewCube.SetActive(false); // Start with the preview cube disabled
+    }
+
+    void UpdatePreviewCubePosition()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(origin.position, origin.forward, out hit) && hit.transform.name == "MapBackground")
+        {
+            Vector3 hitPoint = hit.point;
+            Vector3 snappedPoint = GetSnappedPoint(hitPoint);
+
+            previewCube.transform.position = snappedPoint;
+            previewCube.SetActive(true); // Enable the preview cube
+        }
+        else
+        {
+            previewCube.SetActive(false); // Disable the preview cube if not hitting the plane
         }
     }
 
